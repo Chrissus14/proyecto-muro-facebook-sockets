cf0b762aa5407e0f9331088da5a27e208248d114
Christian Gomez <cjgm14@gmail.com>
2020-03-13 23:41:46 -0600

    codigo de la clase

--- index.js
+++ index.js
@@ -1,11 +1,22 @@
 import express from 'express';
+import socketio from 'socket.io';
+import socketHandler from './src/server/socketHandler';
 
 const app = express();
 
+app.use(express.static('dist'));
+app.set('views', './src/server/views');
+app.set('view engine', 'pug');
+
 app.get('/', (req, res) => {
-  res.send('Hello world');
+  res.render('home');
 });
 
-app.listen(3000, (req, res) => {
+const server = app.listen(3000, (req, res) => {
   console.log('server on port 3000');
 });
+
+const io = socketio(server);
+
+io.set('transport', ['websockets', 'polling']);
+io.on('connection', socketHandler(io));
